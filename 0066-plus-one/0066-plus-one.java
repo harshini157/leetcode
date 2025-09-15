714cb116afc5dc7f93c5af26afe4b602da6e910f
harshini157 <bharshini12b@gmail.com>
2025-09-15 12:31:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (9.7%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -4,10 +4,10 @@ class Solution {
 
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
-                digits[i]++;   // just add 1
-                return digits; // done
+                digits[i]++;   
+                return digits; 
             }
-            digits[i] = 0; // carry over
+            digits[i] = 0; 
         }
 
        
